01fe04b26539d8aa44c0a8e1e6d51d5f728e4c66
nsh9b3 <nsh9b3@mst.edu>
2016-10-06 12:17:15 -0500

    Created an option to take a picture from the app using google camera

--- app/src/main/java/com/stuff/nsh9b3/uface/WebService.java
+++ app/src/main/java/com/stuff/nsh9b3/uface/WebService.java
@@ -2,8 +2,14 @@ package com.stuff.nsh9b3.uface;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,11 +22,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 public class WebService extends Activity
 {
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
+
+
     ArrayList<String> servicesList;
     ListView listViewServices;
     ArrayAdapter servicesAdapter;
@@ -204,5 +218,81 @@ public class WebService extends Activity
         btnTakePic.setVisibility(View.VISIBLE);
         textViewLabelUserId.setVisibility(View.VISIBLE);
         textViewUserID.setVisibility(View.VISIBLE);
+
+        btnTakePic.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // Ensure that there's a camera activity to handle the intent
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                    }
+
+                    if(photoFile.exists())
+                        Log.d("TAG", "File exists");
+                    else
+                        Log.d("TAG", "File doesn't exist");
+
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(getApplicationContext(),
+                                "com.stuff.nsh9b3.uface.fileprovider",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                    }
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_TAKE_PHOTO)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                File file = new File(mCurrentPhotoPath);
+                if(file.exists())
+                {
+                    Log.d("TAG", "File exists");
+                    Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+                    Log.d("TAG", "here");
+                }
+                else
+                    Log.d("TAG", "File doesn't exist");
+            }
+            else
+            {
+                // Back was pressed and no picture was taken
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
